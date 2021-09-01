8c33db1c6414005b49b1d1816d1a28c19dea845a
Konstantin Voykov <koykov0@gmail.com>
2021-09-01 18:15:27 +0300

    Plural issue: update readme.md with description.

--- i18n_test.go
+++ i18n_test.go
@@ -82,13 +82,24 @@ func TestPlural(t *testing.T) {
 	db.SetPolicy(policy.Locked)
 	db.Set("en.user.bag.apples_flag", "You have one apple|You have many apples")
 	db.Set("en.user.bag.apples", "You have !count apple|You have !count apples")
+	db.Set("en.h3.army_size", "[1,5] Few|[5,10] Several|[10,20] Pack|[20,50] Lots|[50,100] Horde|[100,250] Throng|[250,500] Swarm|[500,1000] Zounds|[1000,*] Legion")
 	db.Set("ru.user.bag.apples", "[*,0] У вас проблемы с математикой|{0} У вас нет яблок|{1} У вас !count яблоко|[2,5] У вас !count яблока|[5,21] У вас !count яблок|{21} У вас !count яблоко|[22,25] У вас !count яблока|[25,*] У вас много яблок")
 	db.SetPolicy(policy.LockFree)
 
 	t.Run("en.simple[1]", func(t *testing.T) { testPlural(db, "en.user.bag.apples_flag", "", 1, "You have one apple") })
 	t.Run("en.simple[2]", func(t *testing.T) { testPlural(db, "en.user.bag.apples_flag", "", 2, "You have many apples") })
+
 	t.Run("en.placeholder[1]", func(t *testing.T) { testPlural(db, "en.user.bag.apples", "", 1, "You have 1 apple") })
 	t.Run("en.placeholder[5]", func(t *testing.T) { testPlural(db, "en.user.bag.apples", "", 5, "You have 5 apples") })
+
+	t.Run("en.h3.enemy_size[0]", func(t *testing.T) { testPlural(db, "en.h3.army_size", "N/D", 0, "N/D") })
+	t.Run("en.h3.enemy_size[2]", func(t *testing.T) { testPlural(db, "en.h3.army_size", "", 2, "Few") })
+	t.Run("en.h3.enemy_size[19]", func(t *testing.T) { testPlural(db, "en.h3.army_size", "", 19, "Pack") })
+	t.Run("en.h3.enemy_size[20]", func(t *testing.T) { testPlural(db, "en.h3.army_size", "", 20, "Lots") })
+	t.Run("en.h3.enemy_size[333]", func(t *testing.T) { testPlural(db, "en.h3.army_size", "", 333, "Swarm") })
+	t.Run("en.h3.enemy_size[999]", func(t *testing.T) { testPlural(db, "en.h3.army_size", "", 999, "Zounds") })
+	t.Run("en.h3.enemy_size[1e9]", func(t *testing.T) { testPlural(db, "en.h3.army_size", "", 1e9, "Legion") })
+
 	t.Run("ru.placeholder[-15]", func(t *testing.T) {
 		testPlural(db, "ru.user.bag.apples", "", -15, "У вас проблемы с математикой")
 	})
--- readme.md
+++ readme.md
@@ -16,6 +16,28 @@ db.SetPolicy(policy.LockFree)
 fmt.Println(db.Get("en.messages.welcome"))
 ```
 
+## Pluralization
+
+i18n supports plural formulas. Default formula has format `"<singular>|<plural>"` and supports two ranges: `[0, 1]` for
+singular and `[2, +Inf]` for plural.
+
+In addition to default formulas i18n supports extended formats: `"[low,high] translation|..."`, `"{exact} translation|..."`
+and various combination of them.
+
+Let's pluralize for example [enemy army counts](https://heroes.thelazy.net/index.php/Creature) for Heroes III game:
+```go
+db.Set("en.h3.army_size", "[1,5] Few|[5,10] Several|[10,20] Pack|[20,50] Lots|[50,100] Horde|[100,250] Throng|[250,500] Swarm|[500,1000] Zounds|[1000,*] Legion")
+db.GetPlural("en.h3.army_size", "N/D", 0) // N/D
+db.GetPlural("en.h3.army_size", "", 2) // Few
+db.GetPlural("en.h3.army_size", "", 19) // Pack
+db.GetPlural("en.h3.army_size", "", 20) // Lots
+db.GetPlural("en.h3.army_size", "", 333) // Swarm
+db.GetPlural("en.h3.army_size", "", 999) // Zounds
+db.GetPlural("en.h3.army_size", "", 1e9) // Legion
+```
+
+Check [i18n_test.go](i18n_test.go) to see these examples in action.
+
 ## Transaction support
 
 To reduce lock pressure you may use transaction. See [txn_test.go](txn_test.go) for example.
